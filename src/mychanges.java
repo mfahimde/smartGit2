a121592512fd45fcf7dab33e460bda3900d86b7c
mfahimde <mohammad.devin23@gmail.com>
2021-01-27 12:43:55 -0500

    with changes

--- src/mychanges.java
+++ src/mychanges.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class mychanges {
+public class mychanges {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.printf("added some cod");
+    }
 }
